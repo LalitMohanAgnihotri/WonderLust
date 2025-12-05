b9a254707670fbf7d51438d481f3bca049174821
LalitMohanAgnihotri <lalitagnihotri62@gmail.com>
2025-12-05 20:40:20 +0530

    done

--- app.js
+++ app.js
@@ -1,134 +1,83 @@
 // step1: Import express and mongoose
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const path = require("path");
 
-// set the view engine to ejs
-const path = require('path');
-app.set('views engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// View Engine setup
+const ejsMate = require("ejs-mate");
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-// Middleware to parse urlencoded bodies
+// Middleware
 app.use(express.urlencoded({ extended: true }));
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
-// import the Listing model
-const Listing = require('./models/Listing'); 
+// Public folder setup
+app.use(express.static(path.join(__dirname, "/public")));
 
-// import method-override to support PUT and DELETE methods from forms
-const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
-
-// import ejs 
-const ejsMate=require("ejs-mate");
-app.engine("ejs",ejsMate);
-
-// to use pubilc folder css
-app.use(express.static(path.join(__dirname,"/public")));
-
-// step4: Connect to MongoDB
-
-const dbURI = 'mongodb://127.0.0.1:27017/wanderlust';
+// DB connection
 async function main() {
-    await mongoose.connect(dbURI);
-    console.log('Connected to MongoDB');
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
+main()
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
-main().then(() => {
-    console.log('Database connection established');
-}).catch(err => {
-    console.error('Database connection error:', err);
+// MODEL
+const Listing = require("./models/Listing");
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.redirect("/listings");
 });
 
-// step5: Define routes
-
-// app.get("/testlisting", async(req, res) => {
-//     const newListing = new Listing({
-//         title: "Beautiful Beach House",
-//         description: "A lovely beach house with stunning ocean views.", 
-//         image: {
-//             filename: "beachhouse.jpg",
-//             url: "https://example.com/images/beachhouse.jpg"
-//         },
-//         price: 2500,
-//         location: "Malibu",
-//         country: "USA"
-//     });
-//     await newListing.save();
-//     res.send(newListing);
-// });
-
-// route to get all listings
-app.get("/listings",async(req, res) => {
-    const alllistings=await Listing.find({})
-    res.render("listings/index.ejs", {alllistings});
+// Show all listings
+app.get("/listings", async (req, res) => {
+  const alllistings = await Listing.find({});
+  res.render("listings/index.ejs", { alllistings });
 });
 
-// create new listing form
+// Create form
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
+  res.render("listings/new.ejs");
 });
 
-// post route to create a new listing
-app.post("/listings", async(req, res) => {
-    let {title, description, image, price, location, country} = req.body; 
-    let newListing = new Listing({
-        title: title,
-        description: description,   
-        image:image,
-        price: price,
-        location: location,
-        country: country
-    });
-    await newListing.save();
-    res.redirect("/listings");
-}
-);
-
-// update listing form
-app.get("/listings/:id/update", async(req, res) => {
-    let {id} = req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/update.ejs", {listing});
-});
-// update listing put route
-app.put("/listings/:id", async(req, res) => {
-    let {id} = req.params;      
-    let {title, description, image, price, location, country} = req.body;
-    let updatedListing = await Listing.findByIdAndUpdate(id, {
-        title: title,   
-        description: description,
-        image:image,
-        price: price,
-        location: location,
-        country: country
-    }, {new: true, runValidators: true});
-    res.redirect("/listings"); // Redirect to the listings index page
+// Create route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
-// Delete route
-
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing=await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings")
+// Edit form
+app.get("/listings/:id/update", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/update.ejs", { listing });
 });
 
-// route to get a specific listing by id
-app.get("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+// Update
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, req.body);
+  res.redirect("/listings");
 });
 
-
-// step3: Define a simple route
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// Delete
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 
-
-// step2: Listen on port 8080
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+// Show single listing
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
+
+app.listen(8080, () => console.log("Server running at 8080"));
